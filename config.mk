47695424cf6cf1581feb1dbd3000fcdad6473bb4
Anselm R Garbe <anselm@garbe.us>
2013-05-01 15:39:06 +0200

    shut up about deprecated Xlib functions

--- config.mk
+++ config.mk
@@ -20,7 +20,7 @@ LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS}
 
 # flags
 CPPFLAGS = -D_BSD_SOURCE -D_POSIX_C_SOURCE=2 -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
-CFLAGS = -g -std=c11 -pedantic -Wall -O3 ${INCS} ${CPPFLAGS}
+CFLAGS = -g -std=c11 -pedantic -Wno-deprecated-declarations -Wall -O3 ${INCS} ${CPPFLAGS}
 #CFLAGS = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
 LDFLAGS = -g ${LIBS}
 #LDFLAGS = -s ${LIBS}
